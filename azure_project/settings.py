7b1a44586a3cad4f6c5295bffa375200bc679f50
surawachying-prog <surawach.ying@mail.kmutt.ac.th>
2026-02-05 15:20:10 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
+        'NAME': 'free-sql-db-3382379',
+        'USER': 'surawach-admin',
+        'PASSWORD': 'Ohmoak111@',
+        'HOST': 'surawach-sql-server.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
